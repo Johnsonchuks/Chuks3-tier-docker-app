2f3263aeb7e60b5bdbb057170b3e49efbaca8310
Johnsonchuks <johnsonchuks6@gmail.com>
2026-05-09 11:50:46 +0100

    Add Docker CI/CD workflow

---
+++ Chuks001.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+message="Hello world, my name is Johnson Chuks, but I prefer you put the both name together, which will make it Johnsonchuks. 
+I'm on a juorney of learning cloud computing with TechCrush, with Mr. Temi being our wonderful tutor
+we have been on it for a while now, and it's getting more interesting and tougher by the day
+I promise myself that I will never give up, and I will come out as one of the top students in this class.
+There may be obstacles, but I will never give up, i promise to see this through to the end and beyond,
+So help me God"
+
+echo $message
+
+echo $'check system helth'
+
+#Restarted my cloud computing class
+
+Project="This will be what I will be going into next in the curse of my next days 
+to brush up my cloud computing skills, I will 
+dedicate to it with all my heart to make sure I succeed at the end of it all
+which will be the begining f another phase in my life"
+
+echo $Project
+
